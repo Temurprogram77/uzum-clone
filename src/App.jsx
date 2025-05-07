b309bc28d3fd9f4491726ededac9d48ad69f9087
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-07 19:42:26 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar'
+import Header from './components/header/header'
 
 const App = () => {
   return (
     <div className='bg-white'>
       <Navbar/>
+      <Header/>
     </div>
   )
 }
