3e804aa52a695047bbb92aedb4d0f6ea4b07dadb
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-07 19:56:35 +0300

    added

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,5 +1,8 @@
 const cardsDataBase = [
     {
-        
+        image: "",
+        bigSale: true,
+        original: false,
+        title: "PROKHVOST Mushuklar uchun konserva, losos va alabalk ta'mi, 415 gr"
     },
 ] 
