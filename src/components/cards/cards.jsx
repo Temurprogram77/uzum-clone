c26f857a83f7c2cd0c68650381740b8007eccafe
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-08 17:12:30 +0300

    added

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -3,21 +3,28 @@ import cardsDataBase from "../cardsData";
 
 import img1 from "../../assets/1.jpg";
 import img2 from "../../assets/2.jpg";
+import img3 from "../../assets/3.jpg";
+import img4 from "../../assets/4.jpg";
+import img5 from "../../assets/5.jpg";
 
 const images = {
   "1.jpg": img1,
   "2.jpg": img2,
+  "3.jpg": img3,
+  "4.jpg": img4,
+  "5.jpg": img5,
 };
 
 const Cards = () => {
   return (
-    <div>
+    <div className="max-w-[1255px] mx-auto flex items-center justify-between">
       {cardsDataBase.map((element) => {
         return (
-          <div className="card-box w-[200px] overflow-hidden">
+          <div className="card-box w-[234px] h-[460px] rounded-2xl overflow-hidden">
             <div className="image">
               <img src={images[element.image]} alt={element.title} />
               <p>{element.bigSale && "Katta Savdo"}</p>
+              <p>{element.original && "Original"}</p>
             </div>
             <p className="title">{element.title}</p>
           </div>
--- src/components/cardsData.js
+++ src/components/cardsData.js
@@ -1,16 +1,35 @@
 const cardsDataBase = [
-    {
-        image: `1.jpg`,
-        bigSale: true,
-        original: false,
-        title: "PROKHVOST Mushuklar uchun konserva, losos va alabalk ta'mi, 415 gr"
-    },
-    {
-        image: "2.jpg",
-        bigSale: true,
-        original: false,
-        title: "Крем Floresan F-100 против загара отбеливающий SPF 35, 125 млr"
-    },
-] 
+  {
+    image: `1.jpg`,
+    bigSale: true,
+    original: false,
+    title: "PROKHVOST Mushuklar uchun konserva, losos va alabalk ta'mi, 415 gr",
+  },
+  {
+    image: "2.jpg",
+    bigSale: true,
+    original: false,
+    title: "Крем Floresan F-100 против загара отбеливающий SPF 35, 125 млr",
+  },
+  {
+    image: "3.jpg",
+    bigSale: true,
+    original: true,
+    title: "Бумага листовая Svetocopy ECO, А4ф, 80г/м2, 500 листов",
+  },
+  {
+    image: "4.jpg",
+    bigSale: true,
+    original: false,
+    title: "Qog'oz Snegurochka, ofis, A4, 500 varaq",
+  },
+  {
+    image: "5.jpg",
+    bigSale: true,
+    original: true,
+    title:
+      "Varaq qog'oz, ofis jihozlari uchun Sylvamo Svetocopy, A4f C07/3, 500 varaq",
+  },
+];
 
 export default cardsDataBase;
