a927a189f3cd01c2d6b1b0a6eeae9bd6c5e4c461
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-08 17:01:42 +0300

    added

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import cardsDataBase from "../cardsData";
 
+import img1 from "../../assets/1.jpg";
+import img2 from "../../assets/2.jpg";
+
+const images = {
+  "1.jpg": img1,
+  "2.jpg": img2,
+};
+
 const Cards = () => {
   return (
     <div>
       {cardsDataBase.map((element) => {
         return (
-          <div className="card-box">
+          <div className="card-box w-[200px] overflow-hidden">
             <div className="image">
-              <img src={element.image} alt={element.title} />
+              <img src={images[element.image]} alt={element.title} />
               <p>{element.bigSale && "Katta Savdo"}</p>
             </div>
             <p className="title">{element.title}</p>
--- src/components/cardsData.js
+++ src/components/cardsData.js
@@ -1,12 +1,12 @@
 const cardsDataBase = [
     {
-        image: `https://images.uzum.uz/co6l9ptlqsilsr3ma81g/t_product_540_high.jpg`,
+        image: `1.jpg`,
         bigSale: true,
         original: false,
         title: "PROKHVOST Mushuklar uchun konserva, losos va alabalk ta'mi, 415 gr"
     },
     {
-        image: `https://images.uzum.uz/cc6t06uha88ep89kl1bg/t_product_540_high.jpg`,
+        image: "2.jpg",
         bigSale: true,
         original: false,
         title: "Крем Floresan F-100 против загара отбеливающий SPF 35, 125 млr"
