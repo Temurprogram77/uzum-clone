9a5f25bb825f7a2d3e69f2212bafe1270f0e1158
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-08 16:47:51 +0300

    added

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import cardsDataBase from "./cardsData";
+import cardsDataBase from "../cardsData";
 
 const Cards = () => {
   return (
     <div>
       {cardsDataBase.map((element) => {
-        return <div className="card-box">
-          <div className="image"></div>
-          
-        </div>;
+        return (
+          <div className="card-box">
+            <div className="image">
+              <img src={element.image} alt={element.title} />
+              <p>{element.bigSale && "Katta Savdo"}</p>
+            </div>
+            <p className="title">{element.title}</p>
+          </div>
+        );
       })}
     </div>
   );
---
+++ src/components/cardsData.js
@@ -0,0 +1,16 @@
+const cardsDataBase = [
+    {
+        image: `https://images.uzum.uz/co6l9ptlqsilsr3ma81g/t_product_540_high.jpg`,
+        bigSale: true,
+        original: false,
+        title: "PROKHVOST Mushuklar uchun konserva, losos va alabalk ta'mi, 415 gr"
+    },
+    {
+        image: `https://images.uzum.uz/cc6t06uha88ep89kl1bg/t_product_540_high.jpg`,
+        bigSale: true,
+        original: false,
+        title: "Крем Floresan F-100 против загара отбеливающий SPF 35, 125 млr"
+    },
+] 
+
+export default cardsDataBase;
