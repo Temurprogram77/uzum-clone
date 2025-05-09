ca70536484c4aabeb14442f1189631bbaf1bef54
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-09 10:45:51 +0300

    added

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,16 +1,28 @@
-import React, { useState } from 'react'
-import { motion, AnimatePresence } from 'motion/react'
-import { images } from '../sliderData';
+import React, { useState } from "react";
+import {FaChevronleft} from "react-icons/fa"
+import { motion, AnimatePresence } from "motion/react";
+import { images } from "../sliderData";
 
 const Slider = () => {
-  const [index, setIndex] = useState();
+  const [index, setIndex] = useState(2);
   return (
-    <div className='max-w-[1255px] mx-auto h-[300px] rounded-3xl'>
+    <div className="max-w-[1255px] h-[420px] relative mx-auto rounded-3xl overflow-hidden">
       <AnimatePresence>
-        <motion.img key={images} src='' />
+        <motion.img 
+          key={images} 
+          src={images[index]}
+          initial={{opacity: 0, x: 100}}
+          animate={{opacity: 1, x: 0}}
+          exit={{opacity: 0, x: -100}}
+          transition={{duration: 0.5}}
+          className="object-cover absolute w-full h-full"
+        />
       </AnimatePresence>
+      <button>
+        <FaChevronleft/>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
