257b362225eafb370393afe8e947b72adde19a6e
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-09 16:57:28 +0300

    added react lucide

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
-import Navbar from './components/navbar/navbar'
-import Header from './components/header/header'
-import Cards from './components/cards/cards'
-import Nav from './components/nav/nav'
-import Slider from './components/slider/slider'
-import CardBox from './components/cardBox/cardBox'
-import Pages from './components/pages/pages'
+import React, { useEffect, useState } from "react";
+import { Wifi } from "lucide-react";
+import Navbar from "./components/navbar/navbar";
+import Header from "./components/header/header";
+import Cards from "./components/cards/cards";
+import Nav from "./components/nav/nav";
+import Slider from "./components/slider/slider";
+import CardBox from "./components/cardBox/cardBox";
+import Pages from "./components/pages/pages";
 
 const App = () => {
-  return (
-    <div className='bg-white'>
-      <Nav/>
-      <Navbar/>
-      <Pages/>
-      <Slider/>
-      <Cards/>
-    </div>
-  )
-}
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
 
-export default App
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+  return (
+    <div className="bg-white">
+      <div className={isOnline ? "" : "mx-auto my-28 text-center text-red-900 border-8 w-fit border-red-700 py-3 px-6 rounded-xl"}>
+        {isOnline ? (
+          <>
+            <Nav />
+            <Navbar />
+            <Pages />
+            <Slider />
+            <Cards />
+          </>
+        ) : (
+          <h1 className="status-text">You are currently offline. <Wifi/> </h1>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
