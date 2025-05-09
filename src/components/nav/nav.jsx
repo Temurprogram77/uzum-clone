f0313fa2f580cee28a904f750c968ce321aa3596
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-09 13:33:54 +0300

    added

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import location from "../../assets/location.svg";
 import arrow from "../../assets/arrow.svg";
+import uzb from "../../assets/uzb.png";
 
 const Nav = () => {
   return (
     <div className="w-full bg-[#F0F2F5] py-1">
       <div className="max-w-[1255px] mx-auto flex items-center justify-between">
-        <div className="flex items-center">
+        <div className="flex items-center gap-6">
           <div className="font-medium flex gap-1">
             <img src={location} alt="location" />
             Toshkent
             <img src={arrow} alt="arrow" />
           </div>
           <p className="font-medium text-[14px]">Topshirish punktlari</p>
+        </div>
+        <div className="flex items-center gap-4">
+          <a href="#" className="text-[#8E4DFF] text-[15px] font-medium">Sotuvchi bo'lish</a>
+          <a href="#" className="text-[#8E4DFF] text-[15px] font-medium">Topshirish punktini ochish</a>
+          <a href="#" className="text-[#4D4F59] text-[15px] font-medium">Savol-javob</a>
+          <a href="#" className="text-[#4D4F59] text-[15px] font-medium">Buyurtmalarim</a>
+          <div className="flex items-center gap-2">
+            <img className="w-[20px] h-[20px]" src={uzb} alt="uzb" />
+            O'zbekcha
+          </div>
         </div>
       </div>
     </div>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/uzumLogo.svg";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="max-w-[1255px] mx-auto py-2 flex items-center justify-between">
+      <a href="#">
+        <img src={logo} className="w-[140px] h-[20px]" alt="logo" />
+      </a>
+      <div className=""></div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
