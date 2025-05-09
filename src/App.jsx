d511da8af821791c7d32eadd37dd088d684aa7e1
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-09 20:10:04 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -23,20 +23,57 @@ const App = () => {
       window.removeEventListener("offline", handleOffline);
     };
   }, []);
+
+  useEffect(() => {
+    const scrollHeader = () => {
+      const header = document.getElementById("arrow-btn");
+      if (window.scrollY >= 50) {
+        header.classList.add("bottom-6");
+      } else {
+        header.classList.remove("bottom-6");
+      }
+    };
+
+    window.addEventListener("scroll", scrollHeader);
+
+    // Cleanup function
+    return () => {
+      window.removeEventListener("scroll", scrollHeader);
+    };
+  }, []);
+
   return (
     <div className="bg-white">
-      <div className={isOnline ? "" : "mx-auto my-28 text-center text-red-900 border-8 w-fit border-red-700 py-3 px-6 rounded-xl"}>
+      <div
+        className={
+          isOnline
+            ? ""
+            : "mx-auto my-28 text-center text-red-900 border-8 w-fit border-red-700 py-3 px-6 rounded-xl"
+        }
+      >
         {isOnline ? (
           <>
             <Nav />
             <Navbar />
             <Pages />
             <Slider />
-            <CardBox/>
+            <CardBox />
             <Cards />
+            <div className="arrow-btn p-3 duration-300 fixed -bottom-16 right-6 rounded-full bg-[#7F4DFF] cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width={30}
+                fill="white"
+              >
+                <path d="M12 4.83582L5.79291 11.0429L7.20712 12.4571L12 7.66424L16.7929 12.4571L18.2071 11.0429L12 4.83582ZM12 10.4857L5.79291 16.6928L7.20712 18.107L12 13.3141L16.7929 18.107L18.2071 16.6928L12 10.4857Z"></path>
+              </svg>
+            </div>
           </>
         ) : (
-          <h1 className="status-text flex flex-col items-center">You are currently offline. <Wifi size={40}/> </h1>
+          <h1 className="status-text flex flex-col items-center">
+            You are currently offline. <Wifi size={40} />{" "}
+          </h1>
         )}
       </div>
     </div>
--- src/components/cardBox/cardBox.jsx
+++ src/components/cardBox/cardBox.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import dataCards from '../cardBox'
-
+import React from "react";
+import dataCards from "../cardBox";
 
 const CardBox = () => {
   return (
-    <div className='max-w-[1255px] mx-auto flex justify-between items-center'>
-        {dataCards.map((item)=>{
-            return <div className="bg-[#EFEFEF] px-16 my-4 rounded-xl py-4 flex">
-                <img src={item.image} className='w-[40px]' alt={item.title} />
-                <p>{item.title}</p>
-            </div>
-        })}
+    <div className="max-w-[1255px] mx-auto flex justify-between items-center">
+      {dataCards.map((item) => {
+        return (
+          <div className="bg-[#EFEFEF] hover:bg-[#DEE0E5] duration-150 px-15 cursor-pointer my-4 rounded-xl py-4 flex items-center gap-2">
+            <img src={item.image} className="w-[40px]" alt={item.title} />
+            <p>{item.title}</p>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default CardBox
+export default CardBox;
