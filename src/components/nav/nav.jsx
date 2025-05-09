9c32a754e73022bda0acbbde0e1312bb8c900ad2
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-09 12:11:18 +0300

    added

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import location from  "../../assets/location.svg"
-import arrow from  "../../assets/arrow.svg"
+import location from "../../assets/location.svg";
+import arrow from "../../assets/arrow.svg";
 
 const Nav = () => {
   return (
-    <div className="w-full bg-[#F0F2F5] flex items-center justify-baseline">
-      <div className="max-w-[1255px] mx-auto">
-        <div className="">
-          <div className="">
+    <div className="w-full bg-[#F0F2F5] py-1">
+      <div className="max-w-[1255px] mx-auto flex items-center justify-between">
+        <div className="flex">
+          <div className="flex gap-1">
             <img src={location} alt="location" />
             Toshkent
             <img src={arrow} alt="arrow" />
           </div>
+          <p className="font-normal text-[14px]">Topshirish punktlari</p>
         </div>
       </div>
     </div>
