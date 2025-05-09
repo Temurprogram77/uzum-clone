5a0a1b82c8cc8145b0150cd2d977d67af2938781
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-09 10:33:31 +0300

    adde

---
+++ src/assets/slider.js
@@ -0,0 +1,12 @@
+import img1 from "./slider1.jpg";
+import img2 from "./slider2.jpg";
+import img3 from "./slider3.jpg";
+import img4 from "./slider4.jpg";
+import img5 from "./slider5.jpg";
+export {
+    img1,
+    img2,
+    img3,
+    img4,
+    img5
+};
--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import { motion, AnimatePresence } from 'motion/react'
+import { images } from '../sliderData';
 
 const Slider = () => {
   const [index, setIndex] = useState();
   return (
-    <div className='max-w-[1255px] mx-auto'>
+    <div className='max-w-[1255px] mx-auto h-[300px] rounded-3xl'>
       <AnimatePresence>
-        <motion.img key={images[index]} />
+        <motion.img key={images} src='' />
       </AnimatePresence>
     </div>
   )
