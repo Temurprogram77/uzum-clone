f1c3566f44f723bed2e808340774d09581c4e8d8
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-09 12:12:18 +0300

    added

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -6,13 +6,13 @@ const Nav = () => {
   return (
     <div className="w-full bg-[#F0F2F5] py-1">
       <div className="max-w-[1255px] mx-auto flex items-center justify-between">
-        <div className="flex">
-          <div className="flex gap-1">
+        <div className="flex items-center">
+          <div className="font-medium flex gap-1">
             <img src={location} alt="location" />
             Toshkent
             <img src={arrow} alt="arrow" />
           </div>
-          <p className="font-normal text-[14px]">Topshirish punktlari</p>
+          <p className="font-medium text-[14px]">Topshirish punktlari</p>
         </div>
       </div>
     </div>
