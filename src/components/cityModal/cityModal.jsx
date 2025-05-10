bf7b029fddfc8c4290e17888f9879b67a2bccdc0
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-10 17:13:57 +0300

    added

---
+++ src/components/cityModal/cityModal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import close from "../../assets/close.svg"
+
+const CityModal = () => {
+  return (
+    <div>
+        <h2>Shaharni tanlang</h2>
+        <img src={close} alt="close" />
+    </div>
+  )
+}
+
+export default CityModal
