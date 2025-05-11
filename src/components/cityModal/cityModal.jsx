1fc50f9301eb68b2171248d90adc66015a9f7ef9
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-11 12:12:18 +0300

    added

--- src/components/cityModal/cityModal.jsx
+++ src/components/cityModal/cityModal.jsx
@@ -1,19 +1,42 @@
 import React from "react";
+import locations from "./cityBase";
 
 import close from "../../assets/close.svg";
+import search from "../../assets/search.svg";
 
-const CityModal = ({onClose}) => {
+const CityModal = ({ onClose }) => {
   return (
     <>
-      <div className="w-full h-full bg-[#0000003e] fixed top-0"></div>
-      <div className="">
-        <div className="">
-          <h2>Shaharni tanlang</h2>
-          <img onClick={onClose} src={close} alt="close" />
+      <div
+        onClick={onClose}
+        className="w-full h-full bg-[#00000084] fixed top-0 z-10"
+      ></div>
+      <div className="bg-white w-[660px] fixed z-11 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 rounded-xl">
+        <div className="flex items-center justify-between">
+          <h2 className="text-[22px] font-medium">Shaharni tanlang</h2>
+          <img
+            onClick={onClose}
+            className="cursor-pointer"
+            src={close}
+            alt="close"
+          />
         </div>
-        <input type="text" />
-        <div className="">
-            
+        <div className="w-full h-[500px] overflow-y-scroll my-5">
+          <div className="bg-[#EDEFF2] rounded-xl flex items-center gap-2 mb-3 p-2">
+            <img src={search} alt="search" />
+            <input
+              className="bg-transparent outline-none w-full font-medium"
+              type="text"
+              placeholder="Shaharni topish"
+            />
+          </div>
+          <div className="w-full">
+            {locations.map((location)=>{
+              return <div className="w-full flex items-center justify-between">
+                <p>{location}</p>
+              </div>
+            })}
+          </div>
         </div>
       </div>
     </>
