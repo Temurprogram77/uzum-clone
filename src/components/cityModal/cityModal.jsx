9354251963b2fcae3769e1fe022b0698880efa8a
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-11 11:54:55 +0300

    added

--- src/components/cityModal/cityModal.jsx
+++ src/components/cityModal/cityModal.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import close from "../../assets/close.svg";
 
-const CityModal = () => {
+const CityModal = ({onClose}) => {
   return (
     <>
-      <div className="w-full h-full bg-[#0000003e] fixed"></div>
+      <div className="w-full h-full bg-[#0000003e] fixed top-0"></div>
       <div className="">
         <div className="">
           <h2>Shaharni tanlang</h2>
-          <img src={close} alt="close" />
+          <img onClick={onClose} src={close} alt="close" />
         </div>
         <input type="text" />
         <div className="">
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -7,37 +7,53 @@ import CityModal from "../cityModal/cityModal";
 const Nav = () => {
   const [modal, setModal] = useState(false);
 
-  const openModal = ()=>{
-    return setModal(true)
-  }
+  const openModal = () => {
+    return setModal(true);
+  };
 
-  const closeModal = ()=>{
-    return setModal(false)
-  }
+  const closeModal = () => {
+    return setModal(false);
+  };
 
   return (
-    <div className="w-full bg-[#F0F2F5] py-1">
-      <div className="max-w-[1255px] mx-auto flex items-center justify-between">
-        <div className="flex items-center gap-6">
-          <div onClick={openModal} className="font-medium flex gap-1 cursor-pointer">
-            <img src={location} alt="location" />
-            Toshkent
-            <img src={arrow} alt="arrow" />
+    <>
+      <div className="w-full bg-[#F0F2F5] py-1">
+        <div className="max-w-[1255px] mx-auto flex items-center justify-between">
+          <div className="flex items-center gap-6">
+            <div
+              onClick={openModal}
+              className="font-medium flex gap-1 cursor-pointer"
+            >
+              <img src={location} alt="location" />
+              Toshkent
+              <img src={arrow} alt="arrow" />
+            </div>
+            <p className="font-medium text-[14px] cursor-pointer">
+              Topshirish punktlari
+            </p>
           </div>
-          <p className="font-medium text-[14px] cursor-pointer">Topshirish punktlari</p>
-        </div>
-        <div className="flex items-center gap-4">
-          <a href="#" className="text-[#8E4DFF] text-[15px] font-medium">Sotuvchi bo'lish</a>
-          <a href="#" className="text-[#8E4DFF] text-[15px] font-medium">Topshirish punktini ochish</a>
-          <a href="#" className="text-[#4D4F59] text-[15px] font-medium">Savol-javob</a>
-          <a href="#" className="text-[#4D4F59] text-[15px] font-medium">Buyurtmalarim</a>
-          <div className="flex items-center gap-2 cursor-pointer">
-            <img className="w-[20px] h-[20px]" src={uzb} alt="uzb" />
-            O'zbekcha
+          <div className="flex items-center gap-4">
+            <a href="#" className="text-[#8E4DFF] text-[15px] font-medium">
+              Sotuvchi bo'lish
+            </a>
+            <a href="#" className="text-[#8E4DFF] text-[15px] font-medium">
+              Topshirish punktini ochish
+            </a>
+            <a href="#" className="text-[#4D4F59] text-[15px] font-medium">
+              Savol-javob
+            </a>
+            <a href="#" className="text-[#4D4F59] text-[15px] font-medium">
+              Buyurtmalarim
+            </a>
+            <div className="flex items-center gap-2 cursor-pointer">
+              <img className="w-[20px] h-[20px]" src={uzb} alt="uzb" />
+              O'zbekcha
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {modal && <CityModal onClose={closeModal} />}
+    </>
   );
 };
 
