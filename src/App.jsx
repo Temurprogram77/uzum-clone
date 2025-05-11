c14a487496b6aeb3af8b71e54e71fdbeddebb826
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-11 16:08:37 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -60,48 +60,44 @@ const App = () => {
 
   return (
     <div className="bg-white">
-      {loading ? (
-        <>
-          <Nav />
-          <Navbar />
-          <Pages />
-          <Loading />
-          <Footer />
-        </>
-      ) : isOnline ? (
-        <>
-          <Nav />
-          <Navbar />
-          <Pages />
-          <Slider />
-          <CardBox />
-          <Cards />
-          <AllSeeButton />
-          <Cooler />
-          <Footer />
-          <div
-            ref={headerRef}
-            onClick={scrollToTop}
-            className="arrow-btn p-3 duration-300 fixed -bottom-16 right-6 rounded-full bg-[#7F4DFF] cursor-pointer"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width={30}
-              fill="white"
+      {isOnline ? (
+        loading ? (
+          <>
+            <Nav />
+            <Navbar />
+            <Pages />
+            <Loading />
+            <Footer />
+          </>
+        ) : (
+          <>
+            <Nav />
+            <Navbar />
+            <Pages />
+            <Slider />
+            <CardBox />
+            <Cards />
+            <AllSeeButton />
+            <Cooler />
+            <Footer />
+            <div
+              ref={headerRef}
+              onClick={scrollToTop}
+              className="arrow-btn p-3 duration-300 fixed -bottom-16 right-6 rounded-full bg-[#7F4DFF] cursor-pointer"
             >
-              <path d="M12 4.83582L5.79291 11.0429L7.20712 12.4571L12 7.66424L16.7929 12.4571L18.2071 11.0429L12 4.83582ZM12 10.4857L5.79291 16.6928L7.20712 18.107L12 13.3141L16.7929 18.107L18.2071 16.6928L12 10.4857Z"></path>
-            </svg>
-          </div>
-        </>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width={30}
+                fill="white"
+              >
+                <path d="M12 4.83582L5.79291 11.0429L7.20712 12.4571L12 7.66424L16.7929 12.4571L18.2071 11.0429L12 4.83582ZM12 10.4857L5.79291 16.6928L7.20712 18.107L12 13.3141L16.7929 18.107L18.2071 16.6928L12 10.4857Z"></path>
+              </svg>
+            </div>
+          </>
+        )
       ) : (
-        <div
-          className={
-            isOnline
-              ? ""
-              : "mx-auto my-28 text-center text-red-900 border-8 w-fit border-red-700 py-3 px-6 rounded-xl"
-          }
-        >
+        <div className="mx-auto my-28 text-center text-red-900 border-8 w-fit border-red-700 py-3 px-6 rounded-xl">
           <h1 className="status-text flex flex-col items-center">
             You are currently offline. <Wifi size={40} />
           </h1>
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React from "react";
+import appStore from "../../assets/appStore.svg";
+import playMarket from "../../assets/playMarket.svg";
 
 const Footer = () => {
   return (
     <div>
-      <div className="max-w-[1255px] mx-auto"></div>
+      <div className="max-w-[1255px] mx-auto py-5">
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-2">
+            <h5 className="text-[16px] font-semibold mb-2">Biz haqimizda</h5>
+            <a href="#" className="text-[#8b8e99] text-[14px] font-semibold">
+              Topshirish punktlari
+            </a>
+            <a href="#" className="text-[#8b8e99] text-[14px] font-semibold">
+              Vakansiyalar
+            </a>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h5 className="text-[16px] font-semibold mb-2">Foydalanuvchilarga</h5>
+            <a href="#" className="text-[#8b8e99] text-[14px] font-semibold">
+              Biz bilan bogʻlanish
+            </a>
+            <a href="#" className="text-[#8b8e99] text-[14px] font-semibold">
+              Savol-Javob
+            </a>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h5 className="text-[16px] font-semibold mb-2">Tadbirkorlarga</h5>
+            <a href="#" className="text-[#8b8e99] text-[14px] font-semibold">
+              Uzumda soting
+            </a>
+            <a href="#" className="text-[#8b8e99] text-[14px] font-semibold">
+              Sotuvchi kabinetiga kirish
+            </a>
+            <a href="#" className="text-[#8b8e99] text-[14px] font-semibold">
+              Topshirish punktini ochish
+            </a>
+          </div>
+          <div className="flex flex-col">
+            <h5 className="text-[16px] font-semibold mb-2">Ilovani yuklab olish</h5>
+            <div className="flex gap-5">
+              <a href="#" className="flex">
+                <img src={appStore} alt="apple" />
+                <p>AppStore</p>
+              </a>
+              <a href="#" className="flex">
+                <img src={playMarket} alt="apple" />
+                <p>PlayMarket</p>
+              </a>
+            </div>
+            <h5>Uzum ijtimoiy tarmoqlarda</h5>
+            <div className="flex">
+              <a href="#">
+                <img src={instagram} alt="instagram" />
+                <img src={telegram} alt="telegram" />
+                <img src={facebook} alt="facebook" />
+                <img src={youtube} alt="youtube" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className=""></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
