650e7441fcd0a767e1b7a088422fe4074ebea98a
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-11 15:39:43 +0300

    adde

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -1,19 +1,113 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
   return (
-    <div className='max-w-[1255px]  mx-auto'>
-        <div className="max-w-full mb-3 h-[420px] rounded-xl bg-[#F5F7FA]"></div>
-        <div className="bg-[#F5F7FA] max-w-[300px] h-[30px] rounded-sm"></div>
-        <div className="max-w-full flex flex-wrap justify-between">
-            <div className="bg-[#F5F7FA] w-[234px] h-[300px]"></div>
-            <div className="bg-[#F5F7FA] w-[234px] h-[300px]"></div>
-            <div className="bg-[#F5F7FA] w-[234px] h-[300px]"></div>
-            <div className="bg-[#F5F7FA] w-[234px] h-[300px]"></div>
-            <div className="bg-[#F5F7FA] w-[234px] h-[300px]"></div>
+    <div className="max-w-[1255px]  mx-auto">
+      <div className="max-w-full mb-5 h-[420px] rounded-xl bg-[#F5F7FA]"></div>
+      <div className="bg-[#F5F7FA] max-w-[300px] h-[30px] rounded-sm"></div>
+      <div className="max-w-full flex flex-wrap justify-between mt-5">
+        <div className="my-5">
+          <div className="bg-[#F5F7FA] rounded-sm mb-5 w-[234px] h-[300px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-3 w-[180px] h-[20px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-8 w-[160px] h-[20px]"></div>
+          <div className="flex items-center justify-between">
+            <div className="w-[100px] h-[20px] rounded-sm bg-[#F5F7FA]"></div>
+            <div className="w-[20px] h-[20px] rounded-full bg-[#F5F7FA]"></div>
+          </div>
         </div>
-    </div>
-  )
-}
 
-export default Loading
+        <div className="my-5">
+          <div className="bg-[#F5F7FA] rounded-sm mb-5 w-[234px] h-[300px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-3 w-[180px] h-[20px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-8 w-[160px] h-[20px]"></div>
+          <div className="flex items-center justify-between">
+            <div className="w-[100px] h-[20px] rounded-sm bg-[#F5F7FA]"></div>
+            <div className="w-[20px] h-[20px] rounded-full bg-[#F5F7FA]"></div>
+          </div>
+        </div>
+
+        <div className="my-5">
+          <div className="bg-[#F5F7FA] rounded-sm mb-5 w-[234px] h-[300px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-3 w-[180px] h-[20px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-8 w-[160px] h-[20px]"></div>
+          <div className="flex items-center justify-between">
+            <div className="w-[100px] h-[20px] rounded-sm bg-[#F5F7FA]"></div>
+            <div className="w-[20px] h-[20px] rounded-full bg-[#F5F7FA]"></div>
+          </div>
+        </div>
+
+        <div className="my-5">
+          <div className="bg-[#F5F7FA] rounded-sm mb-5 w-[234px] h-[300px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-3 w-[180px] h-[20px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-8 w-[160px] h-[20px]"></div>
+          <div className="flex items-center justify-between">
+            <div className="w-[100px] h-[20px] rounded-sm bg-[#F5F7FA]"></div>
+            <div className="w-[20px] h-[20px] rounded-full bg-[#F5F7FA]"></div>
+          </div>
+        </div>
+
+        <div className="my-5">
+          <div className="bg-[#F5F7FA] rounded-sm mb-5 w-[234px] h-[300px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-3 w-[180px] h-[20px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-8 w-[160px] h-[20px]"></div>
+          <div className="flex items-center justify-between">
+            <div className="w-[100px] h-[20px] rounded-sm bg-[#F5F7FA]"></div>
+            <div className="w-[20px] h-[20px] rounded-full bg-[#F5F7FA]"></div>
+          </div>
+        </div>
+
+        <div className="my-5">
+          <div className="bg-[#F5F7FA] rounded-sm mb-5 w-[234px] h-[300px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-3 w-[180px] h-[20px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-8 w-[160px] h-[20px]"></div>
+          <div className="flex items-center justify-between">
+            <div className="w-[100px] h-[20px] rounded-sm bg-[#F5F7FA]"></div>
+            <div className="w-[20px] h-[20px] rounded-full bg-[#F5F7FA]"></div>
+          </div>
+        </div>
+
+        <div className="my-5">
+          <div className="bg-[#F5F7FA] rounded-sm mb-5 w-[234px] h-[300px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-3 w-[180px] h-[20px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-8 w-[160px] h-[20px]"></div>
+          <div className="flex items-center justify-between">
+            <div className="w-[100px] h-[20px] rounded-sm bg-[#F5F7FA]"></div>
+            <div className="w-[20px] h-[20px] rounded-full bg-[#F5F7FA]"></div>
+          </div>
+        </div>
+
+        <div className="my-5">
+          <div className="bg-[#F5F7FA] rounded-sm mb-5 w-[234px] h-[300px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-3 w-[180px] h-[20px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-8 w-[160px] h-[20px]"></div>
+          <div className="flex items-center justify-between">
+            <div className="w-[100px] h-[20px] rounded-sm bg-[#F5F7FA]"></div>
+            <div className="w-[20px] h-[20px] rounded-full bg-[#F5F7FA]"></div>
+          </div>
+        </div>
+
+        <div className="my-5">
+          <div className="bg-[#F5F7FA] rounded-sm mb-5 w-[234px] h-[300px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-3 w-[180px] h-[20px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-8 w-[160px] h-[20px]"></div>
+          <div className="flex items-center justify-between">
+            <div className="w-[100px] h-[20px] rounded-sm bg-[#F5F7FA]"></div>
+            <div className="w-[20px] h-[20px] rounded-full bg-[#F5F7FA]"></div>
+          </div>
+        </div>
+
+        <div className="my-5">
+          <div className="bg-[#F5F7FA] rounded-sm mb-5 w-[234px] h-[300px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-3 w-[180px] h-[20px]"></div>
+          <div className="bg-[#F5F7FA] rounded-sm mb-8 w-[160px] h-[20px]"></div>
+          <div className="flex items-center justify-between">
+            <div className="w-[100px] h-[20px] rounded-sm bg-[#F5F7FA]"></div>
+            <div className="w-[20px] h-[20px] rounded-full bg-[#F5F7FA]"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
