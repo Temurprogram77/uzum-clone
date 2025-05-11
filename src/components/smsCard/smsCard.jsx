b965e14d17ca8999b21b293bf901433d5cae662e
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-11 12:34:01 +0300

    added

--- src/components/smsCard/smsCard.jsx
+++ src/components/smsCard/smsCard.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-const SmsCard = () => {
+const SmsCard = ({onClose}) => {
   return (
     <>
       <div className="w-full h-full bg-[#00000084] fixed top-0 z-10"></div>
       <div className="bg-white w-[660px] fixed z-11 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 rounded-xl">
+        <img
+          onClick={onClose}
+          className="cursor-pointer"
+          src={close}
+          alt="close"
+        />
         <h2>Telefon raqamini kiriting</h2>
         <p>Tasdiqlash kodini SMS orqali yuboramiz</p>
         <input type="text" placeholder="+998 00 000-00-00" />
