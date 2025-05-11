f6a1811fb2fb0159061843dd33d6a18e50c3ab71
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-11 15:05:32 +0300

    adde

---
+++ src/components/cooler/cooler.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import cooler from "../../assets/cooler.png";
+
+const Cooler = () => {
+  return (
+    <div className='max-w-[1255px] mx-auto'>
+        <img src={cooler} alt="cooler" />
+    </div>
+  )
+}
+
+export default Cooler
