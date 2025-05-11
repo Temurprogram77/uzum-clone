246a5fc19d43f8765d3b68e812731981db472c30
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-11 14:58:02 +0300

    added

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,43 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/uzumLogo.svg";
 import search from "../../assets/search.svg";
 import user from "../../assets/user.svg";
 import heart from "../../assets/heart.svg";
 import savat from "../../assets/savat.svg";
+import SmsCard from "../smsCard/smsCard";
 
 const Navbar = () => {
+  const [modal, setModal] = useState(false);
+
+  const openModal = () => {
+    return setModal(true);
+  };
+
+  const closeModal = () => {
+    return setModal(false);
+  };
   return (
-    <div className="max-w-[1255px] mx-auto py-2 flex items-center justify-between">
-      <a href="#">
-        <img src={logo} className="w-[200px] h-[30px]" alt="logo" />
-      </a>
-      <div className="flex px-7 py-2 bg-[#E5E5FF] text-[#8D4DFF] font-medium rounded-sm cursor-pointer">
-        Katalog
+    <>
+      <div className="max-w-[1255px] mx-auto py-2 flex items-center justify-between">
+        <a href="#">
+          <img src={logo} className="w-[200px] h-[30px]" alt="logo" />
+        </a>
+        <div className="flex px-7 py-2 bg-[#E5E5FF] text-[#8D4DFF] font-medium rounded-sm cursor-pointer">
+          Katalog
+        </div>
+        <div className="relative flex border-1 border-[#D7D7D9] rounded-sm">
+          <input
+            id="inp"
+            className=" pl-5 w-[450px] outline-none"
+            type="text"
+            placeholder="Mahsulotlar va turkumlar izlash"
+          />
+          <label for="inp" className="cursor-pointer py-2 px-6 bg-[#EDEFF2]">
+            <img src={search} alt="search" />
+          </label>
+        </div>
+        <div
+          onClick={openModal}
+          className="duration-150 flex items-center gap-2 p-2 cursor-pointer hover:bg-[#DEE0E5] rounded-sm"
+        >
+          <img src={user} alt="user" />
+          <p>Kirish</p>
+        </div>
+        <div className="duration-150 flex items-center gap-2 p-2 cursor-pointer hover:bg-[#DEE0E5] rounded-sm">
+          <img src={heart} alt="heart" />
+          <p>Saralangan</p>
+        </div>
+        <div className="duration-150 flex items-center gap-2 p-2 cursor-pointer hover:bg-[#DEE0E5] rounded-sm">
+          <img src={savat} alt="savat" />
+          <p>Savat</p>
+        </div>
       </div>
-      <div className="relative flex border-1 border-[#D7D7D9] rounded-sm">
-        <input
-          id="inp"
-          className=" pl-5 w-[450px] outline-none"
-          type="text"
-          placeholder="Mahsulotlar va turkumlar izlash"
-        />
-        <label for="inp" className="cursor-pointer py-2 px-6 bg-[#EDEFF2]">
-          <img src={search} alt="search" />
-        </label>
-      </div>
-      <div className="duration-150 flex items-center gap-2 p-2 cursor-pointer hover:bg-[#DEE0E5] rounded-sm">
-        <img src={user} alt="user" />
-        <p>Kirish</p>
-      </div>
-      <div className="duration-150 flex items-center gap-2 p-2 cursor-pointer hover:bg-[#DEE0E5] rounded-sm">
-        <img src={heart} alt="heart" />
-        <p>Saralangan</p>
-      </div>
-      <div className="duration-150 flex items-center gap-2 p-2 cursor-pointer hover:bg-[#DEE0E5] rounded-sm">
-        <img src={savat} alt="savat" />
-        <p>Savat</p>
-      </div>
-    </div>
+      {modal && <SmsCard onClose={closeModal} />}
+    </>
   );
 };
 
--- src/components/smsCard/smsCard.jsx
+++ src/components/smsCard/smsCard.jsx
@@ -1,24 +1,39 @@
 import React from "react";
+import close from "../../assets/close.svg";
 
-const SmsCard = ({onClose}) => {
+const SmsCard = ({ onClose }) => {
   return (
     <>
-      <div className="w-full h-full bg-[#00000084] fixed top-0 z-10"></div>
-      <div className="bg-white w-[660px] fixed z-11 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 rounded-xl">
-        <img
-          onClick={onClose}
-          className="cursor-pointer"
-          src={close}
-          alt="close"
+      <div
+        onClick={onClose}
+        className="w-full h-full bg-[#00000084] fixed top-0 z-10"
+      ></div>
+      <div className="bg-white w-[410px] fixed z-11 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 rounded-xl">
+        <div className="flex justify-end w-full">
+          <img
+            onClick={onClose}
+            className="cursor-pointer mb-5 mt-3 "
+            src={close}
+            alt="close"
+          />
+        </div>
+        <h2 className="text-[24px] font-medium">Telefon raqamini kiriting</h2>
+        <p className="text-[16px] font-medium text-[#432026] mb-5">
+          Tasdiqlash kodini SMS orqali yuboramiz
+        </p>
+        <input
+          className="bg-[#EDEFF2] w-full px-3 py-2 text-[20px] font-semibold"
+          type="text"
+          placeholder="+998 00 000-00-00"
         />
-        <h2>Telefon raqamini kiriting</h2>
-        <p>Tasdiqlash kodini SMS orqali yuboramiz</p>
-        <input type="text" placeholder="+998 00 000-00-00" />
-        <button>Kodni olish</button>
-        <p>
+        <button className="bg-[#7000FF] text-[16px] w-full py-2 text-white font-semibold rounded-xl my-6">
+          Kodni olish
+        </button>
+        <p className="mt-16 text-[14px] font-medium tracking-wide leading-4 text-center">
           Avtotizatsiyadan o'tish orqali siz
           <a href="#" className="text-[#3377FF]">
-            shaxsiy ma'lumotlarni qayta ishlash siyosatiga rozilik bildirasiz
+            shaxsiy <br /> ma'lumotlarni qayta ishlash siyosatiga rozilik <br />{" "}
+            bildirasiz
           </a>
         </p>
       </div>
