a1c9f2b3429f007f547de72a28786296f598fd60
Temur Programmer <temurnarzullayev810@gmail.com>
2025-05-15 16:57:09 +0300

    added

---
+++ src/assets/again.js
@@ -0,0 +1,11 @@
+import icon1 from "./bear.png";
+import icon2 from "./verified2.png";
+import icon3 from "./clothes.png";
+import icon4 from "./star.png";
+
+export {
+    icon1,
+    icon2,
+    icon3,
+    icon4
+};
--- src/components/againModal/againModal.jsx
+++ src/components/againModal/againModal.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const AgainModal = () => {
   return (
     <div className='w-full'>
-        <div className="max-w-[1255px] mx-auto"></div>
+        <div className="max-w-[1255px] mx-auto">
+          <div className=""></div>
+          <div className=""></div>
+        </div>
     </div>
   )
 }
